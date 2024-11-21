b8436848ed3c6d7dcc4585c34b235bed155cf208
adhfarg <153331802+adhfarg@users.noreply.github.com>
2024-11-21 18:40:20 -0500

    Matches Screen Icons Added

--- lib/screens/matches_screen.dart
+++ lib/screens/matches_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class MatchesScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> matches = [
+class MatchesScreen extends StatefulWidget {
+  @override
+  _MatchesScreenState createState() => _MatchesScreenState();
+}
+
+class _MatchesScreenState extends State<MatchesScreen> {
+  List<Map<String, dynamic>> matches = [
     {
       'id': 'user1',
       'name': 'Alice',
@@ -34,6 +40,17 @@ class MatchesScreen extends StatelessWidget {
     },
   ];
 
+  void _removeMatch(String id) {
+    setState(() {
+      matches.removeWhere((match) => match['id'] == id);
+      // TODO: Also remove corresponding messages
+    });
+  }
+
+  void _confirmMatch(String id) {
+    // TODO: Implement confirmation logic
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,79 +66,141 @@ class MatchesScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: GridView.builder(
-        padding: EdgeInsets.fromLTRB(
-            12, 12, 12, 80), // Increased bottom padding to avoid overflow
+        padding: EdgeInsets.fromLTRB(12, 12, 12, 80),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 0.75, // Adjusted for better fit
-          crossAxisSpacing: 12, // Reduced spacing
-          mainAxisSpacing: 12, // Reduced spacing
+          childAspectRatio: 0.75,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
         ),
         itemCount: matches.length,
         itemBuilder: (context, index) {
           final match = matches[index];
-          return Card(
-            elevation: 2,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: ClipRRect(
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(12)),
-                    child: Image.asset(
-                      'assets/images/${match['id']}.jpg',
-                      fit: BoxFit.cover,
+          return AnimatedOpacity(
+            duration: Duration(milliseconds: 300),
+            opacity: 1.0,
+            child: Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(12)),
+                          child: Image.asset(
+                            'assets/images/${match['id']}.jpg',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '${match['name']}, ${match['age']}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${match['name']}, ${match['age']}',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 8,
+                                        height: 8,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: match['lastSeen'] == 'Online'
+                                              ? Colors.green
+                                              : Colors.grey,
+                                        ),
+                                      ),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        match['lastSeen'],
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                _buildActionButton(
+                                  onTap: () => _removeMatch(match['id']),
+                                  icon: Icons.close,
+                                  backgroundColor: Colors.red.withOpacity(0.1),
+                                  iconColor: Colors.red,
+                                ),
+                                SizedBox(width: 4),
+                                _buildActionButton(
+                                  onTap: () => _confirmMatch(match['id']),
+                                  icon: Icons.favorite,
+                                  backgroundColor:
+                                      Colors.green.withOpacity(0.1),
+                                  iconColor: Colors.green,
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
-                      SizedBox(height: 4),
-                      Row(
-                        children: [
-                          Container(
-                            width: 8,
-                            height: 8,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: match['lastSeen'] == 'Online'
-                                  ? Colors.green
-                                  : Colors.grey,
-                            ),
-                          ),
-                          SizedBox(width: 4),
-                          Text(
-                            match['lastSeen'],
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildActionButton({
+    required VoidCallback onTap,
+    required IconData icon,
+    required Color backgroundColor,
+    required Color iconColor,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          icon,
+          size: 16,
+          color: iconColor,
+        ),
       ),
     );
   }
